39d78bba89facceeba76c5378fcf24bb0181d535
Luckynamster <Pasharobbot@gmail.com>
2025-04-21 00:55:44 +0500

    добавлены две кнопки: я нашел и я ищу питомца

--- handlers.py
+++ handlers.py
@@ -1,8 +1,9 @@
 from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message, FSInputFile
-import texts
 
+import keyboard_buttons
+import texts
 rt = Router()
 
 
@@ -11,5 +12,7 @@ async def cmd_start(message: Message):
     startPhoto = FSInputFile("images/start_image.png")
     await message.answer_photo(startPhoto,
                                caption=texts.Start,
-                               parse_mode = "HTML"
+                               parse_mode = "HTML",
+                               reply_markup=keyboard_buttons.choose #вызов кнопочек снизу
     )
+
---
+++ keyboard_buttons.py
@@ -0,0 +1,9 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+choose = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="Я НАШЕЛ ПИТОМЦА"), KeyboardButton(text="Я ИЩУ ПИТОМЦА")]
+],
+    resize_keyboard=True,
+    input_field_placeholder="Выберите здесь:" # это меняет текст в поле ввода
+
+)

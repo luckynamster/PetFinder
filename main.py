a1f80e4f5e5f79b95a6571201e1a9e63ca93588e
Wegas <skymag17@gmail.com>
2025-04-20 22:32:46 +0500

    StartCommit

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import asyncio
+import texts
+
 from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
 from config import BOT_TOKEN
 
 dp = Dispatcher()
 bot = Bot(token=BOT_TOKEN)
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer(texts.Start)
+
 
 
 async def main():

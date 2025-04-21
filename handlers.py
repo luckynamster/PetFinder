7e3fc87fb27cc76a45a4a340f00f55b4eae423cb
Luckynamster <Pasharobbot@gmail.com>
2025-04-21 16:48:18 +0500

    добавил запрос фото после нажатия на кнопки

--- handlers.py
+++ handlers.py
@@ -1,18 +1,33 @@
-from aiogram import Router
+from aiogram import Router, F, types
 from aiogram.filters import CommandStart
 from aiogram.types import Message, FSInputFile
 
 import keyboard_buttons
 import texts
+
 rt = Router()
 
 
-@rt.message(CommandStart()) 
+@rt.message(CommandStart())
 async def cmd_start(message: Message):
     startPhoto = FSInputFile("images/start_image.png")
     await message.answer_photo(startPhoto,
                                caption=texts.Start,
-                               parse_mode = "HTML",
-                               reply_markup=keyboard_buttons.choose #вызов кнопочек снизу
-    )
+                               parse_mode="HTML",
+                               reply_markup=keyboard_buttons.choose,  # вызов кнопочек снизу
 
+                               )
+
+
+@rt.message(F.text == "Я ИЩУ ПИТОМЦА")
+async def find_pet(message: Message):
+    await message.reply(texts.FINDPET,
+                        parse_mode="HTML",
+                        reply_markup=types.ReplyKeyboardRemove())
+
+
+@rt.message(F.text == "Я НАШЕЛ ПИТОМЦА")
+async def find_pet(message: Message):
+    await message.reply(texts.FOUNDPET,
+                        parse_mode="HTML",
+                        reply_markup=types.ReplyKeyboardRemove())
--- keyboard_buttons.py
+++ keyboard_buttons.py
@@ -4,6 +4,6 @@ choose = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="Я НАШЕЛ ПИТОМЦА"), KeyboardButton(text="Я ИЩУ ПИТОМЦА")]
 ],
     resize_keyboard=True,
-    input_field_placeholder="Выберите здесь:" # это меняет текст в поле ввода
+    input_field_placeholder="Выберите здесь:", # это меняет текст в поле ввода
 
 )
